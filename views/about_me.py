2acb78638ae4543fcaeff0b17fdb712e492ac3b8
ChandraKasturi <50080824+ChandraKasturi@users.noreply.github.com>
2024-09-21 23:30:30 +0530

    Add files via upload

---
+++ views/about_me.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image("./assets/my_profile_pic.png", width=230)
+
+with col2:
+    st.title("Sri Krishna Kasturi", anchor=False)
+    st.write(
+        "Product / Delivery Manager and AI Evangilist."
+    )
+    if st.button("✉️ Contact Me"):
+        show_contact_form()
+
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write("\n")
+st.subheader("Key Achievements", anchor=False)
+st.write(
+    """
+    - AI Integration for Customer Success: Implemented AI agent, to handle customer queries, reducing support tickets by 20% and increasing client satisfaction. Responsible from idea to execution.
+    - Revenue Boost Cloud Migration: Successfully migrated 3 major clients from on-premises systems to AWS, improving company revenue and reducing operational costs.
+    - Client Retention with Cutting-Edge Tech: Upgraded banking systems to ensure compliance with SWIFT 2021, helping to retain 14 major clients.
+    - Streamlining Product Delivery: Delivered 30+ projects with 70% on-time completion while maintaining high standards of quality and staying within budget.
+    - Award-Winning Leadership: Recognized with the Best Manager Award (CGI, 2017-2018) for successfully delivering the Trade 360 Transformation Project.
+    - New Revenue Streams: Developed a new service offering that contributed to increased company revenue and improved client satisfaction.
+    """
+)
+
+# --- SKILLS ---
+st.write("\n")
+st.subheader("Hard Skills", anchor=False)
+st.write(
+    """
+    - Programming: Python (Scikit-learn, Pandas), SQL, VBA
+    - Data Visualization: PowerBi, MS Excel, Plotly
+    - Modeling: Logistic regression, linear regression, decision trees
+    - Databases: Postgres, MongoDB, MySQL
+    """
+)
